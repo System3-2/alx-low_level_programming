13286783a5332813feee7c5ee6b2abc7ae533ecc
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-10 00:25:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-
 /**
-* main - Entry Point
-*
-* Return : Always 0 (success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
