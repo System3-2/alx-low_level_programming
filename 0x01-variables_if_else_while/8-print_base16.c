6a8282366de38ae4835b03577eb89e1918c17001
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-11 15:06:43 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
