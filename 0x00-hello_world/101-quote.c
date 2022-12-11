60fc8c2c3c676d17ad6aadace06044475f817248
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-11 01:52:32 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	 syscall(SYS_write, 1, msg, sizeof(msg));
+	 return (1);
+}
