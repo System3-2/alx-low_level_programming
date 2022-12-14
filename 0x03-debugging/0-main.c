bb0e5e2d8b9871a88f545a55ee0b3d362da7571d
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-14 15:49:03 -0800

    accept checker

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - tests function that prints is positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
 }
+
