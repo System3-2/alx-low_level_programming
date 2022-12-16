97143c31e5a4768812902cc22805dd6c60d9ccea
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-16 04:08:56 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0123456789
- * owned by Bwave ICT
- * Return: void
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_numbers(void)
 {
-	char c = 0;
+	char c;
 
-	while (c <= 9)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c + '0');
+	_putchar(c);
 	}
 	_putchar('\n');
 }
