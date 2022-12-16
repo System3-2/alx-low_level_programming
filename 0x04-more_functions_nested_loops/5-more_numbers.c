bcde25ff81f43582f09aecd739069427fc0f44cb
OlojaMuyiwa <olojam4969@gmail.com>
2022-12-16 04:11:07 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - number fct
+ * more_numbers - nmbr fct
  * Bwave ICT file
  * Return: no return
  */
+
 void more_numbers(void)
 {
-	int i, j;
 
-	for (i = 0; i <= 10; i++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar('1');
+			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
