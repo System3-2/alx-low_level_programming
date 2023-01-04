0f7571093df9d09a86977c2a0ea5632e155611db
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-04 13:51:23 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,23 +1,25 @@
-
 #include "main.h"
 /**
- * _pow_recursion - power
- * @x:int
- * @y:int
+ * factorial - factorial of int
+ * @n:int
  * Return:int
  */
-
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y != 0)
-		return (x * _pow_recursion(x, y - 1));
+	int x;
 
-	else
+	if (n == 0)
 	{
 		return (1);
 	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+			return (x);
+
 }
